8c2d2c06a250ca217fd3fdbdea107ec5d7c6d9e3
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2016-08-24 22:28:24 +0300

    Use functions for granularity. Dotfiles installation.

--- improvements.sh
+++ improvements.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
 
-# Fix git completion after git updated by brew: remove _git site function
-rm /usr/local/share/zsh/site-functions/_git
+function fix-git-completion() {
+    # Fix git completion after git updated by brew: remove _git site function
+    rm /usr/local/share/zsh/site-functions/_git
+}
+
+function install-dotfiles() {
+    # Install dotfiles (tool to manage config files in home dir)
+    pip install dotfiles
+}
