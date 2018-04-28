a4efc6fb0bbc600aa0de8d19226b81fb861f2b9e
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2018-04-27 18:53:18 -0700

    Add docker images cleanup command

--- .dziman-zsh/docker.enabled.zsh
+++ .dziman-zsh/docker.enabled.zsh
@@ -1,4 +1,10 @@
+#!/bin/zsh
+# Author: Dziman <dziman.by@gmail.com>
+################################################################################
+# Docker shell enhancments
+################################################################################
 alias docker-refresh='eval "$(docker-machine env main-docker)"'
+alias docker-clean-images='docker images --quiet --filter=dangling=true | xargs docker rmi --force'
 
 if [[ $(docker-machine status main-docker 2>/dev/null) = "Running" ]]; then
     eval "$(docker-machine env main-docker)"
