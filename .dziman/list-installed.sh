ed4980f81ea766501d881e8ef382bbed1c1cc4a5
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2018-04-27 19:03:25 -0700

    Use absolute paths in installed packages listing script

--- .dziman/list-installed.sh
+++ .dziman/list-installed.sh
@@ -5,7 +5,7 @@
 # List all installed packages, extensions
 ################################################################################
 
-brew list > brew.packages
-brew cask list > brew.cask.packages
-code --list-extensions > vs.code.extensions
+brew list > ~/.dziman/brew.packages
+brew cask list > ~/.dziman/brew.cask.packages
+code --list-extensions > ~/.dziman/vs.code.extensions
 ################################################################################
