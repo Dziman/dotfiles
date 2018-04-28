e70065ff84f298d06596d7d6d066ffbde5e30a09
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2018-04-27 18:52:33 -0700

    Add AWS shell extension

---
+++ .dziman-zsh/aws.enabled.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Author: Dziman <dziman.by@gmail.com>
+################################################################################
+# AWS related functions
+################################################################################
+
+calculate-s3-bucket-size() {
+    # TODO Add checks for AWS CLI, gnumfmt presence?
+    aws s3api list-object-versions --bucket $1 --query 'Versions[*].[Size,Size]' --output text  | awk '{s+=$1} END {printf "%.0f\n", s}' | gnumfmt --to=iec-i --suffix=B --padding=7
+}
