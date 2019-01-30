306993a17daedda7830bfd6d81780c7cec5d9ed2
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2019-01-29 21:12:39 -0800

    [zsh] Add general development configuration

---
+++ .dziman-zsh/060_develepment.enabled.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# Author: Dziman <dziman.by@gmail.com>
+################################################################################
+# General development tools
+################################################################################
+
+# jenv initialization
+if which anyenv > /dev/null; then eval "$(anyenv init - zsh)"; fi
