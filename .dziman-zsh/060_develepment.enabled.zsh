7b05d33c23ebf32e2d7028515fee5aaa59bcd418
Dzmitry Rusak <drusak@tripactions.com>
2019-09-12 10:51:10 -0700

    Add Node environment management tool (nvm)

--- .dziman-zsh/060_develepment.enabled.zsh
+++ .dziman-zsh/060_develepment.enabled.zsh
@@ -6,3 +6,8 @@
 
 # jenv initialization
 if which anyenv > /dev/null; then eval "$(anyenv init - zsh)"; fi
+
+# node env
+export NVM_DIR="$HOME/.nvm"
+[[ -s "/usr/local/opt/nvm/nvm.sh" ]] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[[ -s "/usr/local/opt/nvm/etc/bash_completion" ]] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion  
