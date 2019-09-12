f7ce0c20a1c4edba5e1788aa28892774686e999b
Dzmitry Rusak <drusak@tripactions.com>
2019-09-12 10:34:35 -0700

    Homebrew configuration: no autoupdate, do not use github

--- .dziman-zsh/004_env.enabled.zsh
+++ .dziman-zsh/004_env.enabled.zsh
@@ -7,7 +7,9 @@
 export ALTERNATE_EDITOR=""
 export EDITOR=emacsclient
 
-export HOMEBREW_GITHUB_API_TOKEN='e80a9e8846522129ddedae42eedd2f81af214cc1'
+# Homebrew configuration
+export HOMEBREW_NO_AUTO_UPDATE="true"
+export HOMEBREW_NO_GITHUB_API="true"
 
 export LSCOLORS=GxFxCxDxBxegedabagaced
 
