03ff852539b2fdd46a38e34e379cb96d0de1f7dc
Dzmitry Rusak <drusak@tripactions.com>
2019-09-12 10:54:53 -0700

    [Docker] Add alias to start docker-machine and setup env variables

--- .dziman-zsh/555_docker.enabled.zsh
+++ .dziman-zsh/555_docker.enabled.zsh
@@ -5,6 +5,7 @@
 ################################################################################
 alias docker-refresh='eval "$(docker-machine env main-docker)"'
 alias docker-clean-images='docker images --quiet --filter=dangling=true | xargs docker rmi --force'
+alias docker-start='docker-machine start main-docker && docker-refresh'
 
 if [[ $(docker-machine status main-docker 2>/dev/null) = "Running" ]]; then
     eval "$(docker-machine env main-docker)"
