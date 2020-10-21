d843e68a3b56447136217e0f6df6f155957c3ac3
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2020-10-21 12:48:37 -0700

    [common scripts] Use updated brew params to get list of installed packages

--- .dziman/list-installed.sh
+++ .dziman/list-installed.sh
@@ -5,8 +5,8 @@
 # List all installed packages, extensions
 ################################################################################
 
-brew list > ~/.dziman/brew.packages
-brew cask list > ~/.dziman/brew.cask.packages
+brew list --formulae > ~/.dziman/brew.packages
+brew list --cask > ~/.dziman/brew.cask.packages
 code --version > /dev/null 2>&1
 if [[ $? == 0 ]]; then
     code --list-extensions > ~/.dziman/vs.code.extensions
