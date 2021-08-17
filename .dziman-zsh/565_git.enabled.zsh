bf52ad38c7b3c914ae9c964708851772ec2cfade
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2021-08-17 15:26:45 -0700

    [zsh] Add forgit support

--- .dziman-zsh/565_git.enabled.zsh
+++ .dziman-zsh/565_git.enabled.zsh
@@ -86,6 +86,20 @@ function git-delete-branch() {
 
 }
 
+function enable-forgit() {
+    if [[ -o interactive ]]; then
+	if [[ -a ~/.dziman-zsh/forgit/forgit.plugin.zsh ]]; then
+            source ~/.dziman-zsh/forgit/forgit.plugin.zsh
+	else
+	    # TODO Add git clone?
+	    # git://github.com/zsh-users/zsh-autosuggestions
+	    echo "forgit not found. Clone from git://github.com/wfxr/forgit to ~/.dziman-zsh/forgit/"
+	fi
+    fi
+}
+
 add-zsh-hook chpwd check-tags-config
 
 [[ -o interactive ]] && check-git-completion
+
+enable-forgit
