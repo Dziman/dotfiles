4f0c1922384925af72e40807a513b4000b2760f5
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2021-08-17 19:23:50 -0700

    [zsh] Update IDEA alias

--- .dziman-zsh/576_idea.enabled.zsh
+++ .dziman-zsh/576_idea.enabled.zsh
@@ -5,7 +5,7 @@
 ################################################################################
 
 function open-idea() {
-    open -na "IntelliJ IDEA.app" --args "$@"
+    open -na "IntelliJ IDEA 2021.x EAP.app" --args "$@"
 }
 
 alias idea=open-idea
