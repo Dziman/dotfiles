a09f579e6c82ef22354286fb357f6abc98d1ce3f
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2022-01-13 19:57:47 -0800

    [brew][zsh] Update list of installed packages. Move helper function

--- .dziman-zsh/999_helpers.enabled.zsh
+++ .dziman-zsh/999_helpers.enabled.zsh
@@ -4,6 +4,9 @@
 # Misc helper functions
 ################################################################################
 
+################################################################################
+# Print terminal color numbers using that color
+################################################################################
 function print-color-palette() {
   for i in {0..255} ; do
     printf "\x1b[38;5;${i}m%3d " "${i}"
@@ -12,3 +15,17 @@ function print-color-palette() {
       fi
   done
 }
+################################################################################
+
+################################################################################
+# List all installed packages, extensions
+################################################################################
+function list-installed() {
+  brew list --formulae > ~/.dziman/brew.packages
+  brew list --cask > ~/.dziman/brew.cask.packages
+  code --version > /dev/null 2>&1
+  if [[ $? == 0 ]]; then
+    code --list-extensions > ~/.dziman/vs.code.extensions
+  fi
+}
+################################################################################
