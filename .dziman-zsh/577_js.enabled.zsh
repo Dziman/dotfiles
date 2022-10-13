6efa5449a1ba82ba1eba9d4683d9f0876d922749
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2022-10-13 10:31:25 -0700

    [zsh] Add NVM support

---
+++ .dziman-zsh/577_js.enabled.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# Author: Dziman <dziman.by@gmail.com>
+################################################################################
+# JavaScript specific
+################################################################################
+
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "/opt/brew/opt/nvm/nvm.sh" ] && \. "/opt/brew/opt/nvm/nvm.sh"
+
+# Completions from zsh-completions package provide better info so this is disabled for now
+## [ -s "/opt/brew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/brew/opt/nvm/etc/bash_completion.d/nvm"
