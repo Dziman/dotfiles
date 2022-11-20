2fcece470dee174cbbeda5bc6f955802a6f391f9
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2022-11-19 22:37:52 -0800

    [zsh] Swift/iOS development

---
+++ .dziman-zsh/578_swift.enabled.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# Author: Dziman <dziman.by@gmail.com>
+################################################################################
+# Swift specific
+################################################################################
+
+# TODO Support some kind of Toolchains switching
+export PATH="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin:$PATH"
