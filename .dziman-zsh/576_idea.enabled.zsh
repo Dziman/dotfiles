2df4d55bbda989dccb2fd9d9ab421e6991d27534
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2023-01-12 12:47:35 -0800

    [zsh] IDEA lounch improvements

--- .dziman-zsh/576_idea.enabled.zsh
+++ .dziman-zsh/576_idea.enabled.zsh
@@ -5,7 +5,15 @@
 ################################################################################
 
 function open-idea() {
-  open -na "IntelliJ IDEA 2021.x EAP.app" --args "$@"
+    if [ -d "/Applications/IntelliJ IDEA 2021.x EAP.app" ]; then
+	open -na "IntelliJ IDEA 2021.x EAP.app" --args "$@"
+    else
+	if [ -d "/Applications/IntelliJ IDEA.app" ]; then
+	    open -na "IntelliJ IDEA.app" --args "$@"
+	else
+	    echo "No installed IDEA found" && return 13
+	fi
+    fi
 }
 
 alias idea=open-idea
