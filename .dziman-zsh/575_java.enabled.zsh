f15ed0c59436741535e39d84b048d816133f2d09
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2024-01-22 13:54:01 -0800

    [zsh] Add jenv versions refresh

--- .dziman-zsh/575_java.enabled.zsh
+++ .dziman-zsh/575_java.enabled.zsh
@@ -7,4 +7,27 @@
 alias mvnp='mvn -T1.5C'
 
 # jenv initialization
-if [[ -o interactive ]] && which jenv > /dev/null; then eval "$(jenv init - zsh)"; fi
+if [[ -o interactive ]] && command-exists jenv; then eval "$(jenv init - zsh)"; fi
+
+function refresh-jenv() {
+  command-exists jenv || return 1
+
+  local -a jenv_versions=($(jenv completions local))
+  jenv_versions=("${(@)jenv_versions:#system}") # remove 'system' entry from versions list
+  jenv_versions=("${(@)jenv_versions:#--unset}") # remove '--unset' entry from versions list
+
+  for jenv_version in $jenv_versions; do
+    jenv remove $jenv_version > /dev/null
+  done
+
+  local brew_prefix=$(brew --prefix)
+  local -a installed_openjdks=($(gls -d /Library/Java/JavaVirtualMachines/*jdk))
+
+  for openjdk_root in $installed_openjdks; do
+      if [[ -L "$openjdk_root" ]]; then
+          jenv add $openjdk_root/Contents/Home/ > /dev/null
+      fi
+  done
+
+  jenv versions
+}
