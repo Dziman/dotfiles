934d9e86f32d0f4def976392ef7b3e92ae7a063b
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2024-11-16 19:57:14 -0800

    [emacs] avy custom face

--- .emacs-dziman/custom.el
+++ .emacs-dziman/custom.el
@@ -6,10 +6,12 @@
  '(custom-safe-themes
     '("fc1275617f9c8d1c8351df9667d750a8e3da2658077cfdda2ca281a2ebc914e0" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(package-selected-packages
-    '(ace-jump-mode ace-jump-helm-line ace-window org-projectile-helm org-projectile ibuffer-projectile helm-org-multi-wiki org-super-agenda helm-org-ql helm-org org-ql org-multi-wiki rainbow-delimiters which-key smartparens smart-mode-line magit labburn-theme idle-highlight-mode helm-projectile editorconfig)))
+    '(helm-colors helm-color rainbow-mode web-mode git-modes ace-jump-mode ace-jump-helm-line ace-window org-projectile-helm org-projectile ibuffer-projectile helm-org-multi-wiki org-super-agenda helm-org-ql helm-org org-ql org-multi-wiki rainbow-delimiters which-key smartparens smart-mode-line magit labburn-theme idle-highlight-mode helm-projectile editorconfig)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(avy-lead-face ((t (:background "#00CED1" :foreground "#00008B" :weight bold))))
+ '(avy-lead-face-0 ((t (:background "#00CED1" :foreground "#FE0000" :weight bold))))
+ '(avy-lead-face-2 ((t (:background "#00CED1" :foreground "#00008B" :weight bold)))))
