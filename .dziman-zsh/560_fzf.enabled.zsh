c19f07511eeeef0622fd6bf2ee1a7c60c3ddc6a1
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2024-11-18 12:06:22 -0800

    [zsh] Add function for `fd` (file finder) `fzf` wrapper

--- .dziman-zsh/560_fzf.enabled.zsh
+++ .dziman-zsh/560_fzf.enabled.zsh
@@ -1,19 +1,39 @@
 if [[ -o interactive ]] && command-exists fzf; then
   eval "$(fzf --zsh)"
 
-  if command-exists rg && command-exists bat; then
-    function rgf() {
-      rg --color=always --line-number --no-heading --ignore-case "${*:-}" |
-        fzf --ansi \
-            --pointer ▶︎ \
-            --marker ✓ \
-            --color "hl:-1:underline,hl+:-1:underline:reverse" \
-            --delimiter : \
-            --preview "bat --color=always {1} --highlight-line {2}" \
-            --preview-window "up,60%,border-bottom,+{2}+3/3,~3" \
-            --bind "alt-enter:become(zsh -i -c 'ideal {2} {1}')" \
-            --bind "enter:become(zsh -i -c 'edit +{2} {1}')"
-    }
+  if command-exists bat; then
+
+    # TODO Review for de-duplication
+    if command-exists rg; then
+      function rgf() {
+        rg --color=always --line-number --no-heading --ignore-case "${*:-}" |
+          fzf --ansi \
+              --pointer ▶︎ \
+              --marker ✓ \
+              --color "hl:-1:underline,hl+:-1:underline:reverse" \
+              --delimiter : \
+              --preview "bat --color=always {1} --highlight-line {2}" \
+              --preview-window "up,60%,border-bottom,+{2}+3/3,~3" \
+              --bind "alt-enter:become(zsh -i -c 'ideal {2} {1}')" \
+              --bind "enter:become(zsh -i -c 'edit +{2} {1}')"
+      }
+    fi
+
+    if command-exists fd; then
+      function fdf() {
+        fd "${*:-}" |
+          fzf --ansi \
+              --pointer ▶︎ \
+              --marker ✓ \
+              --color "hl:-1:underline,hl+:-1:underline:reverse" \
+              --delimiter : \
+              --preview "bat --color=always {1}" \
+              --preview-window "up,60%,border-bottom,+0+3/3,~3" \
+              --bind "alt-enter:become(zsh -i -c 'idea {1}')" \
+              --bind "enter:become(zsh -i -c 'edit {1}')"
+      }
+    fi
+
   fi
 
 fi
