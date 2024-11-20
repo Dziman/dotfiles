f2195858b27e4a36ab2ec8acd3c68128b281cc99
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2024-11-20 13:12:12 -0800

    [zsh] Create `fzf` wrapper with common options like `ctrl-k` to copy in clipboard

--- .dziman-zsh/560_fzf.enabled.zsh
+++ .dziman-zsh/560_fzf.enabled.zsh
@@ -3,38 +3,38 @@ if [[ -o interactive ]] && command-exists fzf; then
 
   if command-exists bat; then
 
+    function dz-fzf() {
+      fzf --ansi \
+          --pointer "▶︎" \
+          --marker "✓" \
+          --color "hl:-1:underline,hl+:-1:underline:reverse" \
+          --bind "ctrl-k:execute(echo {1} | tr -d '\n' | pbcopy)" \
+          --bind "ctrl-o:execute(echo {1})" \
+          "${@}"
+    }
+
     # TODO Review for de-duplication
     if command-exists rg; then
       function rgf() {
         rg --color=always --line-number --no-heading --ignore-case "${*:-}" |
-          fzf --ansi \
-              --pointer ▶︎ \
-              --marker ✓ \
-              --color "hl:-1:underline,hl+:-1:underline:reverse" \
-              --delimiter : \
-              --preview "bat --color=always {1} --highlight-line {2}" \
-              --preview-window "up,60%,border-bottom,+{2}+3/3,~3" \
-              --bind "alt-enter:become(zsh -i -c 'ideal {2} {1}')" \
-              --bind "enter:become(zsh -i -c 'edit +{2} {1}')"
+          dz-fzf --delimiter : \
+                 --preview "bat --color=always {1} --highlight-line {2}" \
+                 --preview-window "up,60%,border-bottom,+{2}+3/3,~3" \
+                 --bind "alt-enter:become(zsh -i -c 'ideal {2} {1}')" \
+                 --bind "enter:become(zsh -i -c 'edit +{2} {1}')"
       }
     fi
 
     if command-exists fd; then
       function fdf() {
         fd "${*:-}" |
-          fzf --ansi \
-              --pointer ▶︎ \
-              --marker ✓ \
-              --color "hl:-1:underline,hl+:-1:underline:reverse" \
-              --delimiter : \
-              --preview "bat --color=always {1}" \
-              --preview-window "up,60%,border-bottom,+0+3/3,~3" \
-              --bind "alt-enter:become(zsh -i -c 'idea {1}')" \
-              --bind "enter:become(zsh -i -c 'edit {1}')"
+          dz-fzf --preview "bat --color=always {1}" \
+                 --preview-window "up,60%,border-bottom,+0+3/3,~3" \
+                 --bind "alt-enter:become(zsh -i -c 'idea {1}')" \
+                 --bind "enter:become(zsh -i -c 'edit {1}')"
       }
     fi
 
   fi
 
 fi
-
