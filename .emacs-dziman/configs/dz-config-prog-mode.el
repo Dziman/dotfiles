e2c350674c9e90cf47a1bec7af74acb4ac48cec1
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2024-11-26 22:01:46 -0800

    [emacs] Remove not working packages. Some cleanup

--- .emacs-dziman/configs/dz-config-prog-mode.el
+++ .emacs-dziman/configs/dz-config-prog-mode.el
@@ -7,8 +7,6 @@
 (use-package smartparens :ensure t)
 (use-package hungry-delete :ensure t)
 (use-package highlight-parentheses :ensure t)
-(use-package whitespace-cleanup-mode :ensure t) ;; TODO Revisit: see no effect
-;; (use-package highlight-indentation :ensure t) ;; TODO Revisit: looks broken
 
 ;; Highlight TODOs
 (setq hl-todo-keyword-faces
@@ -38,10 +36,5 @@
 (defun dz-remove-trailing-whitespace()
   (when (derived-mode-p 'prog-mode)
     (delete-trailing-whitespace)))
-
-;; highlight indentation level
-;; (add-hook 'prog-mode-hook 'highlight-indentation-mode)
-;; (set-face-background 'highlight-indentation-face "#7f7f7f")
-;; (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
 
 (provide 'dz-config-prog-mode)
