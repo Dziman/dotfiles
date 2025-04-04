dfc8c0e2e39fc408d9b6a787fec4c1a10fffa894
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2025-04-04 02:21:33 -0700

    [emacs] Add capability to search across project(ile) files

--- .emacs-dziman/configs/dz-config-projectile.el
+++ .emacs-dziman/configs/dz-config-projectile.el
@@ -4,6 +4,9 @@
 (use-package projectile :ensure t)
 (use-package helm-projectile :ensure t)
 (use-package ibuffer-projectile :ensure t)
+;; TODO Move to common?
+(use-package ag :ensure t)
+(use-package helm-ag :ensure t)
 
 (projectile-mode)
 (setq projectile-completion-system 'helm)
