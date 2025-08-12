01029494b06776c9014b5f6602617c4ebd27f684
Dzmitry Rusak <dzmitry.rusak@gmail.com>
2025-08-12 13:12:05 -0700

    [zsh][java] Update `jenv` refreshing

--- .dziman-zsh/575_java.enabled.zsh
+++ .dziman-zsh/575_java.enabled.zsh
@@ -12,16 +12,11 @@ if [[ -o interactive ]] && command-exists jenv; then eval "$(jenv init - zsh)"; 
 function refresh-jenv() {
   command-exists jenv || return 1
 
-  local -a jenv_versions=($(jenv completions local))
-  jenv_versions=("${(@)jenv_versions:#system}") # remove 'system' entry from versions list
-  jenv_versions=("${(@)jenv_versions:#--unset}") # remove '--unset' entry from versions list
-
-  for jenv_version in $jenv_versions; do
-    jenv remove $jenv_version > /dev/null
-  done
+  setopt localoptions rmstarsilent
+  rm -r  ~/.jenv/versions/*
 
   local brew_prefix=$(brew --prefix)
-  local -a installed_openjdks=($(gls -d /Library/Java/JavaVirtualMachines/*jdk))
+  local -a installed_openjdks=($(gls -d /Library/Java/JavaVirtualMachines/*jdk-[0-9][0-9].jdk))
 
   for openjdk_root in $installed_openjdks; do
       if [[ -L "$openjdk_root" ]]; then
